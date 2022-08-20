1401ad4e412fd846590286921a01b0e027be0173
Mr.Lalit Kumawat <kumawatlalit912@gmail.com>
2022-08-20 19:54:03 +0530

    edited test case and values

--- CSES-Solutions/Sorting and Searching/Concert Tickets.cpp
+++ CSES-Solutions/Sorting and Searching/Concert Tickets.cpp
@@ -1,5 +1,8 @@
+// case#1
+// 5 3
+// 5 3 7 8 5
+// 4 8 3
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int N, M, h, t;
@@ -11,6 +14,13 @@ int main(){
         scanf("%d", &h);
         prices.insert(-h);
     }
+//     output
+    
+//     -8 -7 -5 -5 -3     ----->multiset values 
+//      3
+//      8
+//      -1
+    
     for(int i = 0; i < M; i++){
         scanf("%d", &t);
         if(prices.lower_bound(-t) == prices.end())
